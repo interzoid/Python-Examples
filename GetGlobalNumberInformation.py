9d4c77586df2d70be94266b354fb86be1eba87af
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-08-26 09:17:02 -0700

    Create GetGlobalNumberInformation.py

---
+++ GetGlobalNumberInformation.py
@@ -0,0 +1,10 @@
+# Get API key at www.interzoid.com/register
+import requests
+
+response = requests.get('https://api.interzoid.com/getglobalnumberinfo?license=YOURAPIKEY&intlnumber=+4906979550')
+
+print(response.text)
+
+if response.status_code == 200:
+  data = response.json()
+  print(data["Country"])
