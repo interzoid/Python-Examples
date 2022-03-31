adb401e4b0aa395df65ecf0053c87349a7fd382e
Interzoid <50465512+interzoid@users.noreply.github.com>
2022-03-30 23:05:05 -0700

    Update GetWeatherFromZip.py

--- GetWeatherFromZip.py
+++ GetWeatherFromZip.py
@@ -1,4 +1,4 @@
-# Get API key at www.interzoid.com/register
+# Get API key at www.interzoid.com/register-api-account
 import requests
 
 response = requests.get('https://api.interzoid.com/getweatherzipcode?license=YOURAPIKEY&zip=94111')
