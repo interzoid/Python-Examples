1905b9634be1623c1d78c93bb33fefbe6aab1e70
Interzoid <50465512+interzoid@users.noreply.github.com>
2022-03-30 23:01:10 -0700

    Update GetCompanyMatchSimKey.py

--- GetCompanyMatchSimKey.py
+++ GetCompanyMatchSimKey.py
@@ -1,4 +1,4 @@
-# Get API key at www.interzoid.com/register
+# Get API key at www.interzoid.com/register-api-account
 import requests
 
 response = requests.get('https://api.interzoid.com/getcompanymatchadvanced?license=YOURAPIKEY&company=ibm inc&algorithm=wide')
