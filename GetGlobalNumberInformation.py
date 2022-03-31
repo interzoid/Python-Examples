595d5a9047a907877b2b8ba708c789a4370c1bb2
Interzoid <50465512+interzoid@users.noreply.github.com>
2022-03-30 23:04:16 -0700

    Update GetGlobalNumberInformation.py

--- GetGlobalNumberInformation.py
+++ GetGlobalNumberInformation.py
@@ -1,4 +1,4 @@
-# Get API key at www.interzoid.com/register
+# Get API key at www.interzoid.com/register-api-account
 import requests
 
 response = requests.get('https://api.interzoid.com/getglobalnumberinfo?license=YOURAPIKEY&intlnumber=+4906979550')
